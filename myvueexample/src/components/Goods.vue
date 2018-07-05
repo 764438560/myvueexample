158e15c34c30a21f447fde0d1bededc1d6c334d3
764438560 <764438560@qq.com>
2018-07-05 14:07:04 +0800

    Commit 提交商品详情页等

--- myvueexample/src/components/Goods.vue
+++ myvueexample/src/components/Goods.vue
@@ -3,40 +3,41 @@
   <div class="nav">
     <ul>
       <li v-for="(item, index ) in goodsList" :key="item.index">
-        <p  class="itemTitle">{{item.name}}</p>
-        <div class="goodsWrap" v-for="(its, i) in item.foods" :key="its.i">
-          <div class="itemLeft">
-            <img width="100px" height="100px" :src="its.image" alt="">
-          </div>
-          <div class="itemCenter">
-            <p class="name">{{its.name}}</p>
-            <p class="info">{{its.info}}</p>
-            <p class="price">￥{{its.price}}</p>
-            <p class="sellCount">今日销量：{{its.sellCount}}</p>
-          </div>
-          <div class="itemRight">
-            <div class="cartWrap">
-              <p class="add">+</p>
-              <spa>1</spa>
-              <p class="min">-</p>
-            </div>
+          <p  class="itemTitle">{{item.name}}</p>
+          <div class="goodsWrap" v-for="(its, i) in item.foods" :key="its.i">
 
+            <router-link :to="{path:'/goods/goodsdetail', query:{gInfos:its}}">
+            <div class="itemLeft">
+              <img width="100px" height="100px" :src="its.image" alt="">
+            </div>
+            <div class="itemCenter">
+              <p class="name">{{its.name}}</p>
+              <p class="info">{{its.info}}</p>
+              <p class="price">￥{{its.price}}</p>
+              <p class="sellCount">今日销量：{{its.sellCount}}</p>
+            </div>
+            <div class="itemRight">
+              <div class="cartWrap">
+                <p class="add">+</p>
+                <span>1</span>
+                <p class="min">-</p>
+              </div>
+            </div>
+            </router-link>
+            <div v-if="its.name !=  undefined">
+              <router-view :its="1"></router-view>
+            </div>
           </div>
-        </div>
       </li>
     </ul>
-
   </div>
-  <p>
-    <router-link to="/goods/goodsdetail">商品详情</router-link>
-  </p>
-  <router-view :type="goodsList"></router-view>
+
 </div>
 </template>
 
 <script>
-// import GoodsDedail from './GoodsDedail'
-import GoodsListItems from '../components/GoodsListItem'
+
+
 export default {
   props:['datas'],
   data () {
@@ -50,7 +51,7 @@ export default {
   methods: {
   },
   components: {
-    GoodsListItems
+
   }
 }
 </script>
--- myvueexample/src/components/GoodsDedail.vue
+++ myvueexample/src/components/GoodsDedail.vue
@@ -1,11 +1,28 @@
 <template>
     <div class="goodsDetailContainer">
-        <div class="carousel"></div>
+        <div class="carousel">
+          <img width="100%"  height="100%" :src="gDetails.image" >
+        </div>
         <div class="arrowLeave" @click="goback"></div>
+        <p class="name">{{gDetails.name}}</p>
+        <p class="info">{{gDetails.info}}</p>
+        <p class="price">￥{{gDetails.price}}</p>
+        <p class="sellCount">今日销量：{{gDetails.sellCount}}</p>
     </div>
+
 </template>
 <script>
 export default {
+  props: ['its'],
+  data () {
+    return {
+      gDetails:{}
+    }
+  },
+  mounted () {
+    this.gDetails = this.$route.query.gInfos
+
+  },
   methods: {
     goback(){
       this.$router.go(-1)
